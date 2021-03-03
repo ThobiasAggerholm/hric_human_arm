18893fa17527cfc211152e1d08a53934d211a3cc
Thobias Aggerholm <thagg18@student.sdu.dk>
2021-03-03 13:23:09 +0100

    Revert "Existing human_arm_description and human_arm_gazebo packages"
    This reverts commit 3f0dca4d2740090f02f7e0357eff495a0bbd0c07.

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thobias/Documents/hric/catkin_ws/src
-# Build directory: /home/thobias/Documents/hric/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
